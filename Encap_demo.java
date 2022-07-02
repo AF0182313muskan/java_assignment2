76af768f4417a81ece81e6c1e6853e92b6d410a9
Muskan Shaikh <105120711+AF0182313muskan@users.noreply.github.com>
2022-07-02 14:37:03 +0530

    Add files via upload

---
+++ Encap_demo.java
@@ -0,0 +1,16 @@
+
+public class Encap_demo 
+{
+	private int balance=1000;
+	 
+	public void set_bal(int newval)
+	{
+		balance=balance+newval;
+	}
+
+	public int get_bal()
+	{
+		return balance;
+	}
+	
+}
---
+++ encap2.java
@@ -0,0 +1,15 @@
+
+public class encap2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int bal=300;
+		Encap_demo obj=new Encap_demo();
+		obj.set_bal(bal);
+		System.out.println("The final amount is ="+obj.get_bal());
+
+
+	}
+
+}
